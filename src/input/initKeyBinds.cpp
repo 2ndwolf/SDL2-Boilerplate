9f02cf669af9d0e7e4b5b375c8dfdff713320c2c
Dominic Grenier <themoustafa@hotmail.com>
2021-09-12 14:07:17 -0400

    User defined keybinds now work for realz

--- src/input/initKeyBinds.cpp
+++ src/input/initKeyBinds.cpp
@@ -17,18 +17,11 @@ namespace Input{
   // Keybinds::keys;
 
   void initKeyBinds(std::map<std::string, std::vector<SDL_Scancode>> &iniDefinedKeys){
-
-    // std::map<std::string, std::vector<SDL_Scancode>>::iterator it;
-    // for (it = iniDefinedKeys.begin(); it != iniDefinedKeys.end(); it++){
-    //   Keybinds::keys.insert({it->first, it->second});
-    // }
-
-    // // Take keybinds from user preferences
-    Keybinds::keys["UP"] = {SDL_SCANCODE_UP, SDL_SCANCODE_W};
-    // Keybinds::keys["LEFT"] = {SDL_SCANCODE_LEFT, SDL_SCANCODE_A};
-    // Keybinds::keys["DOWN"] = {SDL_SCANCODE_DOWN, SDL_SCANCODE_S};
-    // Keybinds::keys["RIGHT"] = {SDL_SCANCODE_RIGHT, SDL_SCANCODE_D};
+    
+    std::map<std::string, std::vector<SDL_Scancode>>::iterator it;
+    for (it = iniDefinedKeys.begin(); it != iniDefinedKeys.end(); it++){
+      Keybinds::keys.insert({it->first, it->second});
+    }
   }
-
 
 }
